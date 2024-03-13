236465c1c2bb6a7a70d51bbed8ea6a7d0343c147
Marchello92 <mark.davidovich@gmail.com>
2024-03-13 11:29:37 +0200

    Add basic note.service functionality

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,8 @@
+import { noteService } from "../services/note.service.js"
+
 export function NoteList() {
+    const [notes, setNotes] = useState(null)
+
+    if (!notes) return <div> loading notes </div>
     return <div>note list</div>
 }
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,3 +1,6 @@
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+//hardcoded notes
 const notes = [
     {
         id: 'n101',
@@ -36,3 +39,98 @@ const notes = [
         }
     }
 ]
+
+const NOTE_KEY = 'noteDB'
+
+_createHardCodedNotes(notes)
+
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyNote,
+}
+
+//TODO Filter goes here
+function query() {
+    return storageService.query(NOTE_KEY)
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+        .then(note => _setNextPrevId(note))
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        note = _createNote(note.type, note.info)
+        return storageService.post(NOTE_KEY, note)
+    }
+}
+
+function getEmptyNote(type = '', info = {}) {
+    return {
+        id: '',
+        createdAt: 0,
+        type,
+        isPinned: false,
+        style: {},
+        info,
+    }
+}
+
+
+//TODO filter stuff
+// function getDefaultFilter() {
+
+// }
+
+// function getFilterFromParams() {
+
+// }
+
+//TODO The real deal
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTE_KEY)
+    if (!notes || !notes.length) {
+        notes = []
+        notes.push(_createNote('NoteTxt')) //need more
+    }
+
+}
+
+//! TEMPORARY 
+function _createHardCodedNotes(notes) {
+    const existingNotes = utilService.loadFromStorage(NOTE_KEY)
+
+    if (!existingNotes || !existingNotes.length) {
+        utilService.saveToStorage(BOOK_KEY, notes)
+    }
+}
+
+function _createNote(type = 'NoteTxt', info = { txt: 'Defaultest of notes' }) {
+    const note = getEmptyNote(type, info)
+    note.id = utilService.makeId()
+    note.createdAt = Date.now()
+
+    return note
+}
+
+function _setNextPrevId(note) {
+    return storageService.query(NOTE_KEY).then((notes) => {
+        const carIdx = notes.findIndex((currNote) => currNote.id === note.id)
+        const nextNote = notes[carIdx + 1] ? notes[carIdx + 1] : notes[0]
+        const prevNote = notes[carIdx - 1] ? notes[carIdx - 1] : notes[notes.length - 1]
+        note.nextCarId = nextNote.id
+        note.prevCarId = prevNote.id
+        return note
+    })
+}
