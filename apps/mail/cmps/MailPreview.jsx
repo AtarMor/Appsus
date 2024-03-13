3b77fd4eeba24e1a690b945c3cf2a5e0e044283b
Atar Mor <atarmor92@gmail.com>
2024-03-14 00:11:50 +0200

    Add a star to mail list and render starred mails

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,5 +1,7 @@
 export function MailPreview({ mail }) {
+  const starClass = mail.isStarred ? 'solid yellow-star' : 'fa star'
   return <article className="mail-preview">
+    <div className={starClass}></div>
     <h3 className="mail-from">{mail.from}</h3>
     <h3 className="mail-subject">{mail.subject}</h3>
     <p className="mail-body">{mail.body}</p>
