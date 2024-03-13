d7aa8e39e235c27102ac542a7ae96991d385982a
Atar Mor <atarmor92@gmail.com>
2024-03-13 09:53:41 +0200

    Add basic email model

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,20 @@
-// mail service
+export const mailService = {
+    email,
+    loggedInUser,
+}
+
+const email = {
+    id: 'e101',
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: 'momo@momo.com',
+    to: 'user@appsus.com'
+}
+
+const loggedInUser = {
+    email: 'user@appsus.com',
+    fullName: 'Mahatma Appsus'
+}
