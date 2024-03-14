4c6b1c0beeb73e6fa04c2e3f2c47a4c7373e9cf9
Atar Mor <atarmor92@gmail.com>
2024-03-14 10:25:00 +0200

    Add styling to mail preview

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -5,9 +5,11 @@ import { MailPreview } from "./MailPreview.jsx"
 export function MailList({ mails, onMailSelect }) {
     return <ul className="mail-list clean-list">
         {
-            mails.map(mail => <li onClick={() => onMailSelect(mail.id)} key={mail.id}>
+            mails.map(mail => <li key={mail.id}>
                 <Link to={`/mail/${mail.id}`}>
-                    <MailPreview mail={mail} />
+                    <MailPreview
+                        mail={mail}
+                        onMailSelect={onMailSelect} />
                 </Link>
             </li>)
         }
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,18 +1,19 @@
 import { utilService } from "../../../services/util.service.js"
 
-export function MailPreview({ mail }) {
-  const sentDate = new Date(mail.sentAt) 
-  const formattedDate = sentDate.getDate() + ' ' + utilService.getMonthName(sentDate).substring(0,3)
+export function MailPreview({ mail, onMailSelect }) {
+  const sentDate = new Date(mail.sentAt)
+  const formattedDate = sentDate.getDate() + ' ' + utilService.getMonthName(sentDate).substring(0, 3)
 
   const readClass = mail.isRead ? 'is-read' : ''
 
   const starClass = mail.isStarred ? 'solid yellow-star' : 'fa star'
 
   return <article className={`mail-preview ${readClass}`}>
-    <div className={starClass}></div>
-    <h3 className="mail-from">{mail.from}</h3>
-    <h3 className="mail-subject">{mail.subject}</h3>
-    {/* <p className="mail-body">{mail.body}</p> */}
-    <h3 className="mail-sent-at">{formattedDate}</h3>
+    <div className={starClass} onClick={() => console.log('hi')}></div>
+    <div className="mail-content" onClick={() => onMailSelect(mail.id)}>
+      <h3 className="mail-from">{mail.from}</h3>
+      <h3 className="mail-subject">{mail.subject} - {mail.body}</h3>
+      <h3 className="mail-sent-at">{formattedDate}</h3>
+    </div>
   </article>
 }
