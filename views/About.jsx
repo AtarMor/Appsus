9c31818125ba6e75d03e68b1e87bddc3ad83a5c4
Atar Mor <atarmor92@gmail.com>
2024-03-16 15:50:46 +0200

    Add about page

--- views/About.jsx
+++ views/About.jsx
@@ -1,6 +1,22 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1 className="about-us-header">About us</h1>
+        <p className="welcome-p">Welcome to Appsus, your all-in-one productivity suite designed and developed by our team.
+            Appsus is the culmination of our efforts during the third sprint of our course at Coding Academy, aimed at
+            providing users with a seamless and efficient experience for managing their daily tasks,
+            communication, and more.
+        </p>
+        <h2 className="mission-header">Our Mission</h2>
+        <p className="mission-p">Our Mission is to empower individuals and teams to achieve more by providing them with
+            intuitive and feature-rich applications that streamline their workflow and enhance
+            productivity. We believe that technology should adapt to users' needs, not the other
+            way around. With this philosophy in mind, we strive to deliver innovative solutions
+            that simplify complex tasks and enable users to focus on what matters most.
+        </p>
+        <img className="atar-img" src="assets/img/atar.jpeg" alt="Atar image" />
+        <img className="mark-img"src="assets/img/mark.jpg" alt="Mark image" />
+        <h3 className="atar-name">Atar Mor</h3>
+        <h3 className="mark-name">Mark Davidovich</h3>
     </section>
 }
