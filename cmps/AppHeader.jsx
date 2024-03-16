996f49fbf75efcf86c38b4c54e4d2bfa7f282574
Atar Mor <atarmor92@gmail.com>
2024-03-16 11:34:35 +0200

    Add styling to homepage & app header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,7 +4,7 @@ export function AppHeader() {
 
     return <header className="app-header">
         <Link to="/">
-            <h3>APPSUS!</h3>
+            <logo>Appsus</logo>
         </Link>
         <nav>
             <NavLink to="/">Home</NavLink>
--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,15 @@
 
 export function Home() {
     return <section className="home">
+        <logo>
+            <span>A</span>
+            <span>p</span>
+            <span>p</span>
+            <span>s</span>
+            <span>u</span>
+            <span>s</span>
+        </logo>
         <h1>Welcome to home page!</h1>
+        <footer>Appsus - all rights reserved</footer>
     </section>
 }
